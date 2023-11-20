c9c64edd5cfb9652756840506f3466f1ee76661b
kiminn <134191815+kiminn@users.noreply.github.com>
2023-11-20 21:45:03 +0900

    CRUD

---
+++ src/components/users.js
@@ -0,0 +1,21 @@
+// 배열을 만들어준다
+export const Users = [
+    {
+        id: 1,
+        name: '김성용',
+        nickName: 'zl존성용123',
+        age: 20,
+    },
+    {
+        id: 2,
+        name: '신짱구',
+        nickName: '나는야신짱구',
+        age: 20,
+    },
+    {
+        id: 3,
+        name: '신짱아',
+        nickName: '짱구여동생',
+        age: 20,
+    },
+];
